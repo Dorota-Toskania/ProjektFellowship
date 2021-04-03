6aff5b4d6a38bd1177e8dd3ae698c64d49d6be5b
Dorota <d.gawronska.popa@gmail.com>
2021-04-03 20:14:47 +0200

    Wskażniki zadużenia (od Martyny) i rentownoci do projektu Fellowship

---
+++ Wskazniki_rent_zad_final.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Wskażniki zadłużenia i rentowności
+import pandas as pd
+import numpy as np
+from numpy import inf
+
+data_wsk = pd.read_csv('/Users/dorotagawronska-popa/PycharmProjects/ProjektFellowship/data/df_testy_as_columns.csv  ', index_col = 0)
+
+def convert_period_to_year(period):
+    year = period[-2:]
+    if year[-1] == "*":
+        year = period[-3:-1]
+    if int(year) >= 10:
+        year = "20" + str(year)
+    else:
+        print(period)
+        year = "200" + str(year)
+    # print(year)
+    return year
+
+#convert_period_to_year("Y 12.19")
+
+data_wsk["year"] = data_wsk["period"].map(convert_period_to_year)
+data_wsk['Kapitał Stały'] = data_wsk['I. Kapitał własny'] + data_wsk['1. Zobowiązania długoterminowe']
+data_wsk['Podatek'] = data_wsk['Zysk przed opodatkowaniem'] - data_wsk['V. Zysk netto']
+data_wsk['Nopat'] = data_wsk['III. EBIT'] - data_wsk['Podatek']
+    
+print(data_wsk.columns)
+
+
+# #### Import wskaźniki zadłużenia od Martyny¶
+
+import wskazniki_zadluzenia
+
+aktywa_ogol = data_wsk["Aktywa ogółem"].values
+zob_ogol = data_wsk["II. Zobowiązania ogółem"].values
+zob_dlug = data_wsk["1. Zobowiązania długoterminowe"].values
+kapital_wlasny = data_wsk["I. Kapitał własny"].values
+rsmt = data_wsk["2. Rzeczowe składniki majątku trwałego"].values
+
+# tworzymy nowe kolumny wskaźników zadłużenia
+
+data_wsk["wog"] = wskazniki_zadluzenia.wog(zob_ogol, aktywa_ogol)
+data_wsk["wzkw"] = wskazniki_zadluzenia.wzkw(zob_ogol, kapital_wlasny)
+data_wsk["wdzo"] = wskazniki_zadluzenia.wdzo(zob_dlug, aktywa_ogol)
+data_wsk["wdzkw"] = wskazniki_zadluzenia.wdzkw(zob_dlug, kapital_wlasny)
+data_wsk["wuzd"] = wskazniki_zadluzenia.wuzd(zob_dlug, zob_ogol)
+data_wsk["wpdr"] = wskazniki_zadluzenia.wpdr(rsmt, zob_dlug)
+
+print(data_wsk)
+
+wzkw = data_wsk["wzkw"].values
+kapital_staly = data_wsk['Kapitał Stały'].values
+data_wsk["wsk"] = wskazniki_zadluzenia.wsk(wzkw)
+data_wsk["tsk"] = wskazniki_zadluzenia.tsk(kapital_staly, aktywa_ogol)
+
+# zaokrąglenia
+roa = round(data_wsk['V. Zysk netto']/data_wsk['Aktywa ogółem'],6)*100
+roe = round(data_wsk['V. Zysk netto']/data_wsk['I. Kapitał własny'],6)*100
+roic = round(data_wsk['Nopat']/data_wsk['Kapitał Stały'],6)*100
+ros = round(data_wsk['V. Zysk netto']/data_wsk['I. Przychody ze sprzedaży'],6)*100
+roce = round(data_wsk['III. EBIT']/data_wsk['Kapitał Stały'],6)*100
+
+data_wsk['wog'] = np.round(wskazniki_zadluzenia.wog(zob_ogol, aktywa_ogol), decimals = 6)*100
+data_wsk['wzkw'] = np.round(wskazniki_zadluzenia.wzkw(zob_ogol, kapital_wlasny), decimals = 6)*100
+data_wsk['wdzo'] = np.round(wskazniki_zadluzenia.wdzo(zob_dlug, aktywa_ogol), decimals = 6)*100
+data_wsk['wdzkw'] = np.round(wskazniki_zadluzenia.wdzkw(zob_dlug, kapital_wlasny), decimals = 6)*100
+data_wsk['wuzd'] = np.round(wskazniki_zadluzenia.wuzd(zob_dlug, zob_ogol), decimals = 6)*100
+data_wsk['wpdr'] = np.round(wskazniki_zadluzenia.wpdr(rsmt, zob_dlug), decimals = 6)*100
+
+data_wsk['wsk'] = np.round(wskazniki_zadluzenia.wsk(wzkw), decimals = 6)*100
+data_wsk["tsk"] = np.round(wskazniki_zadluzenia.tsk(kapital_staly, aktywa_ogol), decimals = 6)*100
+
+
+# zamiana nieskończoności
+def zamiana_inf(x):
+    x[x==inf] = 0
+    x[x==-inf] = 0
+    return x
+
+data_wsk['ROA'] = zamiana_inf(roa)
+data_wsk['ROE'] = zamiana_inf(roe)
+data_wsk['ROIC'] = zamiana_inf(roic)
+data_wsk['ROS'] = zamiana_inf(ros)
+data_wsk['ROCE'] = zamiana_inf(roce)
+data_wsk['wog'] = zamiana_inf(data_wsk['wog'])
+data_wsk['wzkw'] = zamiana_inf(data_wsk['wzkw'])
+data_wsk['wdzo'] = zamiana_inf(data_wsk['wdzo'])
+data_wsk['wdzkw'] = zamiana_inf(data_wsk['wdzkw'])
+data_wsk['wuzd'] = zamiana_inf(data_wsk['wuzd'])
+data_wsk['wpdr'] = zamiana_inf(data_wsk['wpdr'])
+data_wsk['wsk'] = zamiana_inf(data_wsk['wsk'])
+data_wsk['tsk'] = zamiana_inf(data_wsk['tsk'])
+
+# print(data_wsk)
+
+# tworzymy plik, tylko na wybrane kolumny
+
+data_wsk = data_wsk[['ticker', 'year','ROA','ROE','ROIC','ROS','ROCE','wog','wzkw','wdzo','wdzkw','wuzd','wpdr','tsk','wsk']].sort_values(by='ticker')
+print(data_wsk)
+
+
+
---
+++ wskazniki_zadluzenia.py
@@ -0,0 +1,186 @@
+
+def wog(zob_ogol: float, aktywa_ogol: float) -> float:
+
+    """
+      Wskaźnik ogólnego zadłużenia (ang. debt ratio)
+
+      Parameters
+      ----------
+      zob_ogol:float
+          Zobowiązania ogółem (ang. Total debt)
+      aktywa_ogol:float
+          Aktywa ogółem (ang. Total assets)
+
+      Returns
+      -------
+      result: float
+          wog
+      """
+    return zob_ogol/aktywa_ogol
+
+
+def wzkw(zob_ogol: float, kapital_wlasny: float) -> float:
+
+    """
+      Wskaźnik zadłużenia kapitału własnego  (ang. Debt to equity ratio)
+
+      Parameters
+      ----------
+      zob_ogol:float
+          Zobowiązania ogółem (ang. Total debt)
+      kapital_wlasny:float
+          Kapitał własny (ang. Total amount of equity)
+
+      Returns
+      -------
+      result: float
+          wzkw
+      """
+#     if kapital_wlasny is None or kapital_wlasny == 0 or zob_ogol is None:
+#         return None
+    #     else:
+    return zob_ogol / kapital_wlasny
+
+
+def wdzo(zob_dlug: float, aktywa_ogol: float) -> float:
+
+    """
+      Wskaźnik długoterminowego zadłużenia ogółem  (ang. Long term debt ratio)
+
+      Parameters
+      ----------
+      zob_dlug:float
+          Zobowiązania długoterminowe (ang. Long term debt)
+      aktywa_ogol:float
+          Aktywa ogółem (ang. Total assets)
+
+      Returns
+      -------
+      result: float
+          wdzo
+      """
+#     if aktywa_ogol is None or aktywa_ogol == 0 or zob_dlug is None:
+#         return None
+#     else:
+    return zob_dlug / aktywa_ogol
+
+
+def wdzkw(zob_dlug: float, kapital_wlasny: float) -> float:
+
+    """
+      Wskaźnik długoterminowego zadłużenia kapitału własnego  (ang. Long term debt to equity ratio)
+
+      Parameters
+      ----------
+      zob_dlug:float
+          Zobowiązania długoterminowe (ang. Long term debt)
+      kapital_wlasny:float
+          Kapitał własny (ang. Total amount of equity)
+
+      Returns
+      -------
+      result: float
+          wdzo
+      """
+#     if kapital_wlasny is None or kapital_wlasny == 0 or zob_dlug is None:
+#         return None
+#     else:
+    return zob_dlug / kapital_wlasny
+
+
+
+def wuzd(zob_dlug: float, zob_ogol: float) -> float:
+
+    """
+      Wskaźnik udziału zobowiązań długoterminowych  (ang. Long term debt to total debt ratio)
+
+      Parameters
+      ----------
+      zob_dlug:float
+          Zobowiązania długoterminowe (ang. Long term debt)
+      zob_ogol:float
+          Zobowiązania ogółem (ang. Total debt)
+
+      Returns
+      -------
+      result: float
+          wuzd
+      """
+#     if zob_ogol is None or zob_ogol == 0 or zob_dlug is None:
+#         return None
+#     else:
+
+    return zob_dlug / zob_ogol
+
+
+
+def wpdr(rsmt: float, zob_dlug: float) -> float:
+
+    """
+      Wskaźnik pokrycia zobowiązań długoterminowymi rzeczowymi składnikami majątku  (ang. The ratio of coverage of liabilities with long-term tangible assets)
+
+      Parameters
+      ----------
+      rsmt:float
+          Rzeczowe składniki majątku trwałego (ang. Tangible fixed assets)
+      zob_dlug:float
+          Zobowiązania długoterminowe (ang. Long term debt)
+
+      Returns
+      -------
+      result: float
+          wpdr
+      """
+#     if zob_dlug is None or zob_dlug == 0 or rsmt is None:
+#         return None
+#     else:
+    return rsmt / zob_dlug
+
+
+def wsk(wzkw: float) -> float:
+
+    """
+    Wskaźnik struktury kapitału  (ang. Capital structure ratio)
+    Wskaźnik struktury kapitału jest odwrotnością wskaźnika zadłużenia kapitału własnego
+    Parameters
+    ----------
+    kapital_wlasny:float
+    Kapitał własny (ang. Total amount of equity)
+    zob_ogol:float
+    Zobowiązania ogółem (ang. Total debt)
+
+    Returns
+    -------
+    result: float
+    wsk
+    """
+#     if wzkw is None or wzkw == 0:
+#         return None
+#     else:
+    return (wzkw**-1)
+
+
+def tsk(kapital_staly: float, aktywa_ogol: float) -> float:
+
+    """
+      Trwałość struktury finansowania  (ang. Capital to assets ratio)
+
+      Parameters
+      ----------
+      kapital_staly:float
+          Kapitał stały (ang. Constant capital)
+      aktywa_ogol:float
+          Aktywa ogółem (ang. Total assets)
+
+      Returns
+      -------
+      result: float
+          tsk
+      """
+#     if aktywa_ogol is None or aktywa_ogol == 0 or kapital_staly is None:
+#         return None
+#     else:
+    return kapital_staly / aktywa_ogol
+
+
+
